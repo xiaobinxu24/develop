d415391d06cd936479357ed42127be5f1b1294cf
xuqunbing <xiaobin_xu@roamter.com>
2016-06-27 13:59:11 +0800

    fix: change the chinese to english

--- kfmanager/mian.c
+++ kfmanager/mian.c
@@ -2,11 +2,11 @@
 	> File Name: kfmanager/mian.c
 	> Author: xiaobin_xu
 	> Mail: xiaobin_xu@melinkr.com 
-	> Created Time: 2016年06月27日 星期一 13时47分49秒
+	> Created Time: 2016-06-27 Monday 13:47:49
  ************************************************************************/
 
 #include<stdio.h>
-int main(int args, char **args)
+int main(int args, char **args)//create main function 
 {
 
 	return 0;
