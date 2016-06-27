ff4f3c321c663e8d454265f4a0f4d8363677f7a5
xuqunbing <xiaobin_xu@roamter.com>
2016-06-27 13:51:59 +0800

    frist: create the mian.c

---
+++ kfmanager/mian.c
@@ -0,0 +1,13 @@
+/*************************************************************************
+	> File Name: kfmanager/mian.c
+	> Author: xiaobin_xu
+	> Mail: xiaobin_xu@melinkr.com 
+	> Created Time: 2016年06月27日 星期一 13时47分49秒
+ ************************************************************************/
+
+#include<stdio.h>
+int main(int args, char **args)
+{
+
+	return 0;
+}
